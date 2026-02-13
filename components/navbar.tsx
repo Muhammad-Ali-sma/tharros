67fbcf5f6b31e0dd65532f639cc854011e41c0e8
Muhammad-Ali-sma <syedalisma1234@gmail.com>
2026-02-13 09:02:47 +0500

    init

---
+++ components/navbar.tsx
@@ -0,0 +1,49 @@
+import { navItems } from '@/lib/constants'
+import { User } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { NavigationMenu } from 'radix-ui'
+import { NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from './ui/navigation-menu'
+import { Button } from './ui/button'
+
+export const Navbar = () => {
+    return (
+        <header className="flex items-center justify-center py-6 px-16 border-b gap-16 border-white/20">
+            <Image src={'/logo.png'} alt='Logo' width={103} height={26} />
+            <NavigationMenu.Root>
+                <NavigationMenuList>
+                    {navItems.map((item =>
+                        <NavigationMenuItem key={item.name}>
+                            {item.submenu ?
+                                <>
+                                    <NavigationMenuTrigger>{item.name}</NavigationMenuTrigger>
+                                    <NavigationMenuContent className='border p-1 rounded-md'>
+                                        <ul>
+                                            {item.submenu.map((subitem =>
+                                                <li key={subitem.name}>
+                                                    <NavigationMenuLink asChild className='p-1.5 text-foreground hover:bg-accent-foreground hover:text-accent'>
+                                                        <Link href={subitem.to}>{subitem.name}</Link>
+                                                    </NavigationMenuLink>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </NavigationMenuContent>
+                                </>
+                                :
+                                <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                                    <Link href={item.to}>{item.name}</Link>
+                                </NavigationMenuLink>
+                            }
+                        </NavigationMenuItem>
+                    ))}
+                </NavigationMenuList>
+            </NavigationMenu.Root>
+            <div className='flex items-center gap-5'>
+                <Button variant={'ghost'} size={'xs'}>
+                    <User />  Login
+                </Button>
+                <Button size={'xs'}>Dashboard</Button>
+            </div>
+        </header>
+    )
+}
---
+++ lib/constants.ts
@@ -0,0 +1,54 @@
+export const navItems = [
+    {
+        name: 'Your Stories',
+        to: '#',
+    },
+    {
+        name: 'Challenges',
+        to: '#',
+    },
+    {
+        name: 'Ambassadors',
+        to: '#',
+    },
+    {
+        name: 'Shop',
+        to: '#',
+    },
+    {
+        name: 'Inspiration Hub',
+        to: '#',
+        submenu: [
+            {
+                name: 'Content Hub',
+                to: '#',
+            },
+            {
+                name: 'Blog',
+                to: '#',
+            },
+            {
+                name: 'Podcast',
+                to: '#',
+            }
+        ]
+    },
+    {
+        name: 'About Us',
+        to: '#',
+        submenu: [
+            {
+                name: 'Vision',
+                to: '#',
+            },
+            {
+                name: 'Our Story',
+                to: '#',
+            },
+            {
+                name: 'Contact Us',
+                to: '#',
+            }
+        ]
+    }
+]
